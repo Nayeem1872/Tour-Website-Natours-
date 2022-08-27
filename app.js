43786dce12486526ac44886ed5d8abf1dab94c9d
Nayeem1872 <nayeem.islam.1872@gmail.com>
2022-08-27 11:22:50 +0600

    This is a new commit for what I originally planned to be amended

--- app.js
+++ app.js
@@ -1,125 +1,31 @@
 const express = require('express');
-const fs = require('fs');
-const app = express ();
+const morgan = require('morgan');
 
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
-app.use((req,res,next)=>{
-    console.log('Hello Middleware');
-    next();
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
-app.use((req, res, next)=>{
-    req.requestTime = new Date().toISOString();
-    next();
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
- const tours= JSON.parse(
-    fs.readFileSync(`${__dirname}/data/tours-simple.json`)
- );
 
- const getAllTours = (req, res)=> {
-    console.log(req.requestTime);
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-    res.status(200).json({
-        status : 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-
- };
-
- const getTour= (req, res)=>{
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.fins(el => el.id === id);
-
-    if(!tour){
-        return res.status(404).json({
-            status:' fail',
-            massage: 'invalid ID'
-        });
-}
-    res. status(200).json({
-        status:'sucess',
-        data :{
-            tour
-        }
-    });
-
- };
-
- const createTour = (req, res)=> {
-
-    const newId =tours[tours.length-1].id+1;
-    const newTour= object.assign({ id:newId}, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-        res.status(201).json({
-            status :'success',
-            data: {
-                tour:newTour
-            }
-        });
-    }
-    );
-
- };
-
- const updateTour = (req, res) =>{
-    if(req.params.id *1>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'invalid ID'
-        });
-    }
-    res.status(200).json({
-        status:'success',
-        data:{
-            tour:'<updated tour here........>'
-        }
-    });
- };
-
- const deleteTour = (req, res)=>{
-    if(req.params.id *1 > tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'Invalid ID'
-        });
-    }
-    res,status(204).json({
-        status:'success',
-        data: null
-    });
- };
-
-
- app
-    .route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour);
-app
-    .route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
-
-
-
-// app.get("/", (req, res)=> {
-
-//     res.status(200).send("Helllo i am nayeem");
-// })
-const port = 3000;
-app.listen(port, ()=>{
-    console.log(`app is running ${port}`);
-
-}
-)
+module.exports = app;
