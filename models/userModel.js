62a5ed05a4b34ea242d10c96da2d2b86d269fc33
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2022-09-15 00:24:52 -0700

    Updating

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8,
+    minlength: 6,
     select: false
   },
   passwordConfirm: {
